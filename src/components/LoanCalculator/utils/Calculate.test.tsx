15719db619931679745ee192dbbe971df62e0f67
Eric Hoover <ehoover3@yahoo.com>
2023-01-29 15:31:34 -0500

    updated projects page to card(s)

--- src/components/LoanCalculator/utils/Calculate.test.tsx
+++ src/components/LoanCalculator/utils/Calculate.test.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-
 import calculate from "./Calculate";
 
 describe("Loan Calculator", () => {
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,46 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function Projects() {
   return (
-    <div>
-      <div>Projects</div>
-      <Nav.Link href='./loancalculator'>Loan Calculator</Nav.Link>
+    <div style={{ margin: "5%" }}>
+      <h1>Projects</h1>
+      <Nav.Link href='./loancalculator'>
+        <Card style={{ width: "18rem" }}>
+          {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              height: "100px",
+              backgroundColor: "#58cc02",
+              color: "white",
+              fontWeight: "bold",
+              fontSize: "75px",
+            }}
+          >
+            %
+          </div>
+          <Card.Body>
+            <Card.Title style={{ fontWeight: "bold", textAlign: "center" }}>
+              Loan Calculator
+            </Card.Title>
+            <Card.Text>
+              <div>
+                Enter loan amount, term, and rate to calculate the monthly payment, total interest,
+                & total paid
+              </div>
+              <br />
+              <div style={{ fontWeight: "bold", fontStyle: "italic" }}>Technical Skills</div>
+              <div>React useState and useEffect</div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Nav.Link>
     </div>
   );
 }
