d90576805a5d856c8591237f49620ff6c8338d64
Eric Hoover <ehoover3@yahoo.com>
2023-01-31 17:31:44 -0500

    new branch dev

--- src/components/LoanCalculator/components/Results.tsx
+++ src/components/LoanCalculator/components/Results.tsx
@@ -16,8 +16,8 @@ export default function Results({ loan }: any) {
       {
         label: "# of Votes",
         data: [0, 0],
-        backgroundColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
-        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        backgroundColor: ["rgba(100,100,100,1)", "rgba(200, 200, 200, 1)"],
+        borderColor: ["rgba(100,100,100,1)", "rgba(200, 200, 200, 1)"],
         borderWidth: 0,
       },
     ],
